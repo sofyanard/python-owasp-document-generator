3518ea433f6708ff4d413c123ffe165b2f48ba94
socrates965 <socrates.sullivan@gmail.com>
2024-07-16 10:00:07 +0700

    Update main.py
    add multiple document process
    add concurrent process

--- main.py
+++ main.py
@@ -1,36 +1,54 @@
 from read_write_json import read_json_file, write_json_file
 from read_write_docx import initialize_doc, insert_to_doc, add_header, load_doc, merge_and_save_docx, replace_text_in_docx
-import math
-import asyncio
 import time
 from tkinter import Tk     # from tkinter import Tk for Python 3.x
-from tkinter.filedialog import askopenfilename
+import tkinter.filedialog as fd
+import concurrent.futures
 
 def main():
     Tk().withdraw() # we don't want a full GUI, so keep the root window from appearing
-    filename = askopenfilename(filetypes=[("json files", "*.json")]) # show an "Open" dialog box and return the path to the selected file
-        
-    data= read_json_file(filename)
-    if not data or "dependencies" not in data:
-        print("wrong file selected")
-        return False
+    filename=fd.askopenfilenames(title='Choose JSON File',filetypes=[("json files", "*.json")])
+    start = time.time()
     
-    base_data_dir = './data/'
-    report_dir = base_data_dir + 'generated_report/'
-    failed_data_dir = base_data_dir + 'failed/'
+    with concurrent.futures.ThreadPoolExecutor(max_workers=4) as executor:
+        data = executor.map(get_files, filename)
     
+    with concurrent.futures.ProcessPoolExecutor(max_workers=4) as executor:
+        result = executor.map(generate_report, data)
+    
+    for r in result:
+        print(r)
+    
+    end = time.time()
+    print("Total Elapsed time: " + str(end-start))
+    
+def get_files(filename):
+    try:
+        json = read_json_file(filename)
+        if not json or "dependencies" not in json:
+            print("wrong file selected")
+
+        return json
+    except Exception as err:
+        print(f'Error reading file: {filename}. {repr(err)}')
+        # print("Error reading file: ", err)
+        pass
+    
+def generate_report(data):
     doc_template_open_name = "./data/doc_template/Reporting-SAKTI-opening-template.docx"
     doc1= load_doc(doc_template_open_name)
     doc_template_close_name = "./data/doc_template/Reporting-SAKTI-closing-template.docx"
     doc2= load_doc(doc_template_close_name)
     
+    base_data_dir = './data/'
+    report_dir = base_data_dir + 'generated_report/'
+    failed_data_dir = base_data_dir + 'failed/'
     ArtifactName= data["projectInfo"]["name"]
     replace_text_in_docx(doc1, "@%APP%@", ArtifactName)
+    dependencies = data["dependencies"]
     failed=[]
 
     start = time.time()
-    
-    dependencies = data["dependencies"]
     for dependency in dependencies:
 
         if "vulnerabilities" in dependency:
@@ -46,30 +64,18 @@ def main():
 
                 if insert_to_doc(vulnerability, doc1, i, ArtifactName) == False:
                     failed.append(vulnerability)
-                i+=1
-    
-    
-    header2 = {}
-    # for vulnerability in sorted_vulnerabilities:
-    #     if vulnerability["Severity"] not in header2:
-    #         add_header(vulnerability["Severity"], doc1, 2)
-    #         header2[vulnerability["Severity"]] = True
-    #     
-    #     if insert_to_doc(vulnerability, doc1, i, ArtifactName) == False:
-    #         failed.append(vulnerability)
-    #     i+=1
-
+                i+=1   
     doc1.add_page_break()
     
     merge_and_save_docx(doc1, doc2, report_dir + "Depedency scan "+ArtifactName+".docx")
+    doc_dir = report_dir + "Depedency scan "+ArtifactName+".docx"
     
     if failed:
         print("All failed task stored in " + failed_data_dir + "Failed Append "+ArtifactName+".json")
+        print(f'elapsed time: {time.time()-start}')
         write_json_file(failed, failed_data_dir + "Failed Append "+ArtifactName+".json")
-    
-    end = time.time()
-    print("Elapsed time: " + str(end-start))
-    
+    else:
+        return doc_dir + " is generated successfully in " + str(time.time()-start)
 
 def truncate_text(text, max_length, ellipsis=True):
     if len(text) > max_length:
